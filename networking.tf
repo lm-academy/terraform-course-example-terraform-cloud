6ddef393f2bfc05ba15347b4f1ac0709ead9e8ed
Lauro Fialho Müller <contact@lauromueller.com>
2024-03-29 13:53:49 +0100

    refactor(networking): migrate to private networking module

--- networking.tf
+++ networking.tf
@@ -1,16 +1,26 @@
-resource "aws_vpc" "tf_cloud" {
-  cidr_block = var.vpc_cidr
-
-  tags = {
-    Name = "terraform-cloud"
-  }
+moved {
+  from = aws_vpc.tf_cloud
+  to   = module.networking-tf-course.aws_vpc.this
 }
 
-resource "aws_subnet" "tf_cloud" {
-  cidr_block = var.subnet_cidr
-  vpc_id     = aws_vpc.tf_cloud.id
+moved {
+  from = aws_subnet.tf_cloud
+  to   = module.networking-tf-course.aws_subnet.this["subnet_1"]
+}
 
-  tags = {
-    Name = "terraform-cloud"
+module "networking-tf-course" {
+  source  = "app.terraform.io/LauroMueller/networking-tf-course/aws"
+  version = "0.1.1"
+
+  vpc_config = {
+    cidr_block = var.vpc_cidr
+    name       = "terraform-cloud"
+  }
+
+  subnet_config = {
+    subnet_1 = {
+      cidr_block = "10.0.0.0/24"
+      az         = "eu-west-1a"
+    }
   }
 }
